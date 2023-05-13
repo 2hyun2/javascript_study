d2e433999d03f22604796fab3a894f583fd7cb90
이정현 <eventietter@naver.com>
2023-05-13 16:45:42 +0900

    공부를 시작함

---
+++ main.js
@@ -0,0 +1,30 @@
+// number
+console.log(123);
+console.log(123 + 456, 123 * 456);
+
+// numbrer + string...???
+console.log(123 + "456"); // => 579? "123456"?
+
+// string
+console.log("Hello, World!");
+console.log("Hello, " + "World!");
+
+// boolean
+console.log(true, false);
+console.log(!true, !false);
+
+// symbol
+console.log(Symbol("a"));
+
+// typeof
+console.log(typeof 123); // => number
+// Not a Number
+console.log(typeof NaN); // => number
+console.log(typeof Infinity); // => number
+console.log(typeof "Hello, World!"); // => string
+console.log(typeof true); // => boolean
+console.log(typeof Symbol("a")); // => symbol
+console.log(typeof {}); // => object
+console.log(typeof []); // => object
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
